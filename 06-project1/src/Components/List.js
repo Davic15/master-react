daf69995c452c4cca6a99a99d4f55ab70c6c0ec7
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-07-07 14:25:27 +0200

    Project using React.js - Movies

---
+++ 06-project1/src/Components/List.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+
+export const List = () => {
+  return (
+    <Fragment>
+      <article className="movie-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">New Movie</p>
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+      <article className="movie-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">New Movie</p>
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+      <article className="movie-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">New Movie</p>
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+      <article className="movie-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">New Movie</p>
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+    </Fragment>
+  );
+};

7be5e8ddb6d8838536cb0bf9850a14fbb64c83f2
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-01-22 20:03:38 +0100

    Checking again React.

--- 08-project2/src/Components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    return (
-        <footer className='footer'>
-            Portfolio Franklin David Macias Avellan &copy; 2022
-        </footer>
-    )
-}
--- 13-context/src/Context/Context.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-
-export const ContextWork = createContext(null);
